8cf22f77c928febfd5739faf3a89dcad28f42e99
user7 <kuldeepd@cybage.com>
2015-01-16 14:44:44 +0530

    adlk

---
+++ src/main/java/test/cybage/CoreVaibhav/App.java
@@ -0,0 +1,13 @@
+package test.cybage.CoreVaibhav;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
